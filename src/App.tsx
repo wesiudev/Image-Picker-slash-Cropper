a0bb615ecd910e7a5c60080aea793c30f10f8ddd
wesiudev <wesselpawel@wp.pl>
2022-11-01 17:47:54 +0100

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,46 @@
-import gavelBlack from './images/gavelBlack.png';
+import gavelBlack from "./images/gavelBlack.png";
 import userBlack from "./images/userBlack.png";
 import scrollBlack from "./images/scrollBlack.png";
 import heartBlack from "./images/heartBlack.png";
 import gearBlack from "./images/gearBlack.png";
 import Header from "./components/Header";
 import NavItem from "./components/NavItem";
-import Profile from './components/Profile/Profile';
-import { useEffect, useState } from 'react';
+import Profile from "./components/Profile/Profile";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  
-  const [isMenuVisible, setMenuVisible] = useState<boolean>(false);
-  const width = window.innerWidth
-  console.log(width)
-    useEffect(() => {
-      if(width > 1025 && !isMenuVisible){
-        setMenuVisible(true)
-      }else{
-        setMenuVisible(false)
-      }
-    }, [width, setMenuVisible])
+  const [isMenuVisible, setMenuVisible] = useState<boolean>(true);
   return (
     <>
       <Header />
       <div className="interfaceWrapper">
-      <nav>
-        {isMenuVisible === true ?
-          <ul>
-            <NavItem
-              image={userBlack}
-              itemName="Profil"
-            />
-            <NavItem
-              image={scrollBlack}
-              itemName="Ogłoszenia"
-            />
-            <NavItem
-              image={heartBlack}
-              itemName="Ulubione"
-            />
-            <NavItem
-              image={gavelBlack}
-              itemName="Opinie"
-            />
-            <NavItem
-              image={gearBlack}
-              itemName="Ustawienia konta"
-            />
-            <li>
-              <button>
-                Wyloguj
-              </button>
-            </li>
-          </ul>
-          : null}
-          {width < 1025 ?  <button onClick={() => setMenuVisible(!isMenuVisible)} className='mobileMenuOpen'>M</button> : null}
-        
-        </nav> 
+        <nav>
+          {isMenuVisible === true ? (
+            <ul>
+              <NavItem image={userBlack} itemName="Profil" />
+              <NavItem image={scrollBlack} itemName="Ogłoszenia" />
+              <NavItem image={heartBlack} itemName="Ulubione" />
+              <NavItem image={gavelBlack} itemName="Opinie" />
+              <NavItem image={gearBlack} itemName="Ustawienia konta" />
+              <li>
+                <button>Wyloguj</button>
+              </li>
+            </ul>
+          ) : null}
+          <button
+            onClick={() => setMenuVisible(!isMenuVisible)}
+            className="mobileMenuOpen"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </nav>
         <div className="feed">
           <div className="feed__basicValues">
-            <div className='section0'><Profile/></div>
+            <div className="section0">
+              <Profile />
+            </div>
           </div>
         </div>
       </div>
